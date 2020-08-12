128d2c42892eb02c1cb9776dcf82db3d0b3b3e0a
Preston Haddock <quotablesarcasm@gmail.com>
2020-08-11 21:07:49 -0600

    Stupid simple site nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,29 @@
 <template>
+
   <div>
+
+    <div class="container">
+      
+      <div class="row">
+
+        <div class="col-4">
+          <nuxt-link to="/">Home</nuxt-link>
+        </div>
+
+        <div class="col-4">
+          <nuxt-link to="/blog">Blog</nuxt-link>
+        </div>
+
+        <div class="col-4">
+          <nuxt-link to="/resume">Resume</nuxt-link>
+        </div>
+
+      </div> <!-- !.row -->
+
+    </div> <!-- !.container -->
+
     <Nuxt />
+
   </div>
+
 </template>
