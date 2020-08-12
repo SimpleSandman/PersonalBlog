44bc5ac36fcb764a9f7a369da509d024e327a44a
Preston Haddock <quotablesarcasm@gmail.com>
2020-08-11 20:09:05 -0600

    Simple blog page placeholder

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{ post.fields.title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: `${this.$store.state.title} | ${this.post.fields.title}`,
+        }
+    },
+    data() {
+        return {
+            slug: this.$route.params.slug,
+        }
+    },
+    computed: {
+        post() {
+            return this.$store.state.posts.find(post => post.fields.slug === this.slug);
+        }
+    }
+}
+</script>
