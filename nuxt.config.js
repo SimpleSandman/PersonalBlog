0e3d56b570134460cef4cfeb234c5420dc64216c
Preston Haddock <quotablesarcasm@gmail.com>
2020-08-11 20:24:33 -0600

    Added contentful config to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const contentful = require('contentful');
+/*
+    @TODO: Figure out what it is complaining that accessToken is not provided
+    even though it still properly fetches data.
+*/
+const client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    accessToken: process.env.CONTENTFUL_ACCESSTOKEN
+});
+
 export default {
   /*
   ** Nuxt rendering mode
