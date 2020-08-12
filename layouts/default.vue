d9e5fe3fa2aaaf9dd0c010dc690bb1becb8669fb
Preston Haddock <quotablesarcasm@gmail.com>
2020-08-11 21:50:35 -0600

    Added super simple mobile nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,15 @@
 
     <div class="main-nav container">
       
-      <div class="row">
+      <label for="hamburg" class="mobile-only hamburg">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+
+      <input type="checkbox" @click.prevent="" id="hamburg">
+
+      <div class="nav-row row">
 
         <div class="col-4">
           <nuxt-link to="/">Home</nuxt-link>
@@ -29,7 +37,38 @@
 </template>
 
 <style lang="scss" scoped>
+  @import '~/scss/_mixins';
   .main-nav {
-    padding-top: 1.25rem;
+    padding-top: 1.25rem;    
+    #hamburg {
+      display: none;
+    }
+    @include media-screen(700px) {
+      .nav-row {
+        display: none;
+      }
+      .hamburg {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        height: 25px;
+        span {
+          display: block;
+          width: 25px;
+          height: 3px;
+          margin-bottom: 2px;
+          background-color: #333;
+        }
+      }
+      #hamburg:checked ~ .nav-row {
+        display: block;
+      }
+    }
   }
 </style>
+
+<script>
+export default {
+  
+}
+</script>
