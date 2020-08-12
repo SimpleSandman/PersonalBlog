21c6813afb5c551bf7a50a47de280b98a04aaafc
Preston Haddock <quotablesarcasm@gmail.com>
2020-08-11 20:08:55 -0600

    Super simple placeholder for listing posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    <h1>My Cool Site</h1>
-  </div>
+<div class="container">
+  
+  <h1>My Cool Site</h1>
+
+  <template v-for="post in posts">
+
+    <div :key="post.sys.id">
+      <h3>
+        <nuxt-link :to="postUrl(post.fields.slug)">{{ post.fields.title }}</nuxt-link>
+      </h3>
+    </div>
+
+
+  </template>
+
+</div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    }
+  },
+  methods: {
+    postUrl(slug) {
+      return `/blog/${slug}`;
+    }
+  }
 }
 </script>
