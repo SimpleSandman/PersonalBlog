2a67d7b5a6e60f8e6a982eab2ec4bd472116fcfa
Preston Haddock <quotablesarcasm@gmail.com>
2020-08-11 18:42:57 -0600

    Adding nuxt files

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'og:title', content: 'Preston Haddock | Wild Beard' },
+      { name: 'og:description', content: 'Preston Haddock, Wild Beard, develops things for the web.' },
+      { name: 'og:url', content: 'https://wildbeard.app' },
+      { name: 'og:image', content: '/images/Le-Beard.png' },
+      { name: 'description', content: 'Preston Haddock, AKA Wild Beard, develops things for the web.' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'ie=edge' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: '/images/Le-Beard.png' },
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+  }
+}
